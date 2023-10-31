f9c685bb9cdad19865a19ffb3176f5ee595aba6f
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-01 01:28:52 +0600

    routing system for app route

---
+++ src/app/news/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewsPage = () => {
+  return (
+    <div>
+      <h1 className="text-5xl">This is News Page </h1>
+    </div>
+  );
+};
+
+export default NewsPage;
